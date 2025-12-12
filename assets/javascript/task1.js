509066cddd6d4640d1c7d512501bd490d2cbd738
Zac <zachary.geraghty@vtsd.com>
2025-12-12 09:24:15 -0500

    Quiz

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,8 @@
 /* Put code for the task below */
+function functionName(){
+    document.getElementById("buttonTask1").style.display="";
+}
+function imageHide(){
+    document.getElementById("imageTask1").style.display="none";
+}
+//<button style="display:none" id="buttonTask1">Click Me</button>
