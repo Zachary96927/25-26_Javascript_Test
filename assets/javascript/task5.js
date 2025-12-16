ce8dab97f3fe1205bc0a54a096ec5e059e177f9f
Zac <zachary.geraghty@vtsd.com>
2025-12-16 07:52:42 -0500

    Test Submit

--- assets/javascript/task5.js
+++ assets/javascript/task5.js
@@ -1,4 +1,5 @@
 /* Put code for the task below */
+//Checks box to see if it is blank or not
 function checkBox1(){
     string1=document.getElementById("fullName").value;
     blank= "";
@@ -8,25 +9,25 @@ function checkBox1(){
         document.getElementById("fullName").style.backgroundColor="white"
 }
 function checkBox2(){
-    string1=document.getElementById("address").value;
+    string2=document.getElementById("address").value;
     blank= "";
-    if (string1 === blank) {
+    if (string2 === blank) {
         document.getElementById("address").style.backgroundColor="red"
     } else
         document.getElementById("address").style.backgroundColor="white"
 }
 function checkBox3(){
-    string1=document.getElementById("phone").value;
+    string3=document.getElementById("phone").value;
     blank= "";
-    if (string1 === blank) {
+    if (string3 === blank) {
         document.getElementById("phone").style.backgroundColor="red"
     } else
         document.getElementById("phone").style.backgroundColor="white"
 }
 function checkBox4(){
-    string1=document.getElementById("email").value;
+    string4=document.getElementById("email").value;
     blank= "";
-    if (string1 === blank) {
+    if (string4 === blank) {
         document.getElementById("email").style.backgroundColor="red"
     } else
         document.getElementById("email").style.backgroundColor="white"
